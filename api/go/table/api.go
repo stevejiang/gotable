4d8070ea7295f47faf6f78c19f8257eb3f6519b8
stevejiang <stevejiang@stevejiang-virtual.(none)>
2015-01-10 13:04:24 +0800

    Update api package desc

--- api/go/table/api.go
+++ api/go/table/api.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The client API of GoTable. GoTable is a high performance NoSQL database.
-
+// The official client API of GoTable. GoTable is a high performance
+// NoSQL database.
 package table
 
 import (
